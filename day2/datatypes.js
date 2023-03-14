761453900d0396618c0ffa3c4ffd23bb140b45fe
githubscientist <sathish.datascientist@gmail.com>
2023-03-14 20:24:15 +0530

    day2 - datatypes updated

---
+++ day2/datatypes.js
@@ -0,0 +1,90 @@
+// data types
+/*
+    1. Number
+        - integer
+        - floating point
+
+    2. String
+        - sequence of characters
+
+    3. Boolean
+        - true
+        - false
+
+    4. Object
+        - Array
+        - JavaScript Object
+
+    5. Miscelaneous
+        - null
+        - NaN
+        - undefined
+*/
+
+// let age = 25;
+// console.log(typeof (age));
+
+// let price = 35.64;
+// console.log(typeof (price));
+
+// let fruitName = 'apple is a fruit';
+// let password = "Apple@123";
+// let chocolateName = `bounty`;
+
+// // console.log(chocolateName);
+// console.log(typeof (fruitName), typeof(password), typeof(chocolateName));
+
+// let isEligible = false;
+
+// console.log(isEligible, typeof(isEligible));
+
+// class of 10 students scores in Javascript out of 100
+
+// let scores = [45, 12, 18, 56, 75, 98, 100, 74, 36, 10];
+
+// console.log(scores[9]);
+
+// let columnNames = ['name', 'age', 'salary', 'location', 'isEligible', 'scores', 'percentage'];
+// let person1 = ['vijay', 25, 25000, 'tamilnadu', true, [12, 15, 30], 85.43];
+
+// // console.log(columnNames[5], person1[5][2]);
+
+// console.log(typeof (person1));
+
+// console.log(scores.slice(0, 1).concat(scores.slice(2,)));
+
+// let person1 = {
+//     name: 'vijay',
+//     age: 25,
+//     salary: 25000, 
+//     location: 'tamilnadu',
+//     isEligible: true,
+//     scores: [12, 15, 30],
+//     percentage: 85.43
+// };
+
+// // console.log(person1.percentage);
+// // console.log(typeof (person1));
+
+// console.log(person1);
+
+// let name = 'krish';
+
+// console.log(name == null ? 'no name' : name);
+
+// let number = NaN;
+
+// console.log(isNaN(number));
+
+// let number;
+// console.log(number);
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// console.log(numbers[6]);
+
+// console.log(10 / 'abc');
+
+// console.log(Math.sqrt(-1));
+
+// console.log(parseInt('abc'));
---
+++ day2/variables.js
@@ -0,0 +1,14 @@
+// let number = 5;
+
+// variables are changeables
+number = 10;
+number = 10;
+
+number = 15;
+
+console.log(number);
+
+number = 20;
+
+console.log(number);
+
