928f2ac20eb8c010c2c86f4511df5ac34fce52d0
githubscientist <sathish.datascientist@gmail.com>
2023-03-15 09:42:23 +0530

    tasks updated --copy by value and copy by reference

--- day3/tasks.js
+++ day3/tasks.js
@@ -29,4 +29,38 @@ let myName = 'sathish';
 // 8. Declare a variable called 'myUndefined' and do not assign any value to it.
 
 
+// 9. Create a variable called myNumber and assign it a value of 10. 
+// Create another variable called myNumberCopy and assign it the value of myNumber.
+// Change the value of myNumber to 20. 
+// Log the values of myNumber and myNumberCopy to the console.
+// What do you expect to see ? Why ?
+
+
+// 10. Create an array called myArray and assign it an array of numbers. 
+// Create another variable called myArrayCopy and assign it the value of myArray.
+// Change the first value of myArray to 100. Log the values of myArray and myArrayCopy to the console.
+// What do you expect to see ? Why ?
+
+
+
+// 11. Create an object called myObject with several properties. 
+// Create another variable called myObjectCopy and assign it the value of myObject.
+// Change one of the properties of myObject.
+// Log the values of myObject and myObjectCopy to the console.What do you expect to see ? Why ?
+
+
+
+// 12. Create a variable called myString and assign it a string value. 
+// Create another variable called myStringCopy and assign it the value of myString.
+// Change the value of myString.Log the values of myString and myStringCopy to the console.
+// What do you expect to see ? Why ?
+
+
+
+// 13. Create a variable called myBoolean and assign it a boolean value. 
+// Create another variable called myBooleanCopy and assign it the value of myBoolean.
+// Change the value of myBoolean.Log the values of myBoolean and myBooleanCopy to the console.
+// What do you expect to see ? Why ?
+
+
 
